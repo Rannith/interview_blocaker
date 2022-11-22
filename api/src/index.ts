2b3e56cb4a655dd111bbcaccfe27b1f69575a481
Rannith <kmrannithvikram451@gmail.com>
2022-11-22 13:32:20 +0530

    Response wrapper and error handler

--- api/src/index.ts
+++ api/src/index.ts
@@ -3,10 +3,14 @@ import cors from 'cors'
 import userRouter from './route/userRoute'
 import bookingRouter from './route/bookingRoute'
 import './database'
+import ResponseWrapper from './utils/responseWrapper'
+import { NextFunction, Request, Response } from 'express';
+import ErrorHandler from './utils/errorHandler'
 
 const app = express()
 
 const port = process.env.PORT
+const responseWrapper = new ResponseWrapper()
 
 app.use(json())
 app.use(cors())
@@ -14,5 +18,14 @@ app.use(cors())
 app.use('/user', userRouter)
 app.use('/booking', bookingRouter)
 
+app.use(new ErrorHandler().handleErrors)
+// app.use(function (err: {status: number, message: Object}, req: Request, res: Response, next: NextFunction) {    
+//     if(err.status) {
+//             res.status(err.status).json(responseWrapper.error(err.message, err.status))
+//         }else {
+//             res.status(500).json(responseWrapper.error(err, res.statusCode))
+//         }
+// })
+
 // app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`App listening on port ${port}!`))
