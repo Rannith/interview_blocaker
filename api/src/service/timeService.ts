afe1c1dccc7f6fa50d68bede56dabde27d0767d8
Rannith <kmrannithvikram451@gmail.com>
2022-11-30 12:58:53 +0530

    time validator

--- api/src/service/timeService.ts
+++ api/src/service/timeService.ts
@@ -11,6 +11,14 @@ class TimeService {
     public checkAvailableTime = async (payload: { date: any; startTime: any; endTime: any; }) => {
         const { date, startTime, endTime } = payload;
         let result
+        const inStartTime = new Date(config.dateValue + dayjs(startTime).format('HH:mm'))
+        const inEndTime = new Date(config.dateValue + dayjs(endTime).format('HH:mm'))
+
+        if(inStartTime >= inEndTime) {
+            throw ({ status: 400, message: `Start time ust be less than end time, your selection ${dayjs(inStartTime).format('hh:mm A')} to ${dayjs(inEndTime).format('hh:mm A')}` })
+        } else if(dayjs(inEndTime).diff(dayjs(inStartTime)) <= config.minimumMinute) {
+            throw ({ status: 400, message: `Book slot minimum 30 minutes, you booked ${dayjs(inEndTime).diff(dayjs(inStartTime))/60000} minutes only` })
+        }
 
         const checkAvailableTime = await this.timeRepository.checkAvailableTime(Booking, date, startTime, endTime)
 
@@ -18,15 +26,13 @@ class TimeService {
             const startedTime = new Date(config.dateValue + dayjs(element.startTime).format('HH:mm'))
             const endedTime = new Date(config.dateValue + dayjs(element.endTime).format('HH:mm'))
 
-            const inStartTime = new Date(config.dateValue + dayjs(startTime).format('HH:mm'))
-            const inEndTime = new Date(config.dateValue + dayjs(endTime).format('HH:mm'))
-
             if (startedTime <= inStartTime && inStartTime <= endedTime
                 || startedTime <= inEndTime && inEndTime <= endedTime
                 || inStartTime <= startedTime && startedTime <= inEndTime
                 || inStartTime <= endedTime && endedTime <= inEndTime
-                ) {                    
-                result = { message: `you have booked time slot of ${dayjs(element.startTime).format('h:mm A')} to ${dayjs(element.endTime).format('h:mm A')},please booked another time slot` }
+            ) {
+                // result = { message: `you have booked time slot of ${dayjs(element.startTime).format('h:mm A')} to ${dayjs(element.endTime).format('h:mm A')},please booked another time slot` }
+                throw ({ status: 400, message: `you have booked time slot of ${dayjs(element.startTime).format('h:mm A')} to ${dayjs(element.endTime).format('h:mm A')},please book another time slot` })
             }
         })
 
--- api/src/utils/config.ts
+++ api/src/utils/config.ts
@@ -1,1 +1,2 @@
 export const dateValue: string = "1/1/2011 "
+export const minimumMinute: number = 1800000
